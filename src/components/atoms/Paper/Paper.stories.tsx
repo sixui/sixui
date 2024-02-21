c77dd41cf9fbe8782163c9bdac53436e5777a10c
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-02-21 10:30:19 +0400

    Typography now has onSurface as default color role

--- src/components/atoms/Paper/Paper.stories.tsx
+++ src/components/atoms/Paper/Paper.stories.tsx
@@ -9,7 +9,6 @@ import {
 } from '@/components/utils/ComponentShowcase';
 import { Paper, type IPaperProps } from './Paper';
 import { paperVariants } from './Paper.styledefs';
-import { colorRolesVars } from '@/themes/base/vars/colorRoles.stylex';
 import { Typography } from '../Typography';
 
 // https://material.io/blog/tone-based-surface-color-m3
@@ -43,7 +42,6 @@ const paperContentStyles = stylex.create({
     justifyContent: 'flex-end',
     padding: '16px',
     gap: '16px',
-    color: colorRolesVars.onSurface,
   },
 });
 
