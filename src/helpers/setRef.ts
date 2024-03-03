0e51a42aff8678a6be339473f1c72c9017e711c0
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-03-03 21:53:13 +0400

    chore: cleanup

--- src/helpers/setRef.ts
+++ src/helpers/setRef.ts
@@ -1,15 +1,17 @@
 // https://github.com/mui/material-ui/blob/master/packages/mui-utils/src/setRef.ts
 
 /**
- * passes {value} to {ref}
+ * passes `value` to `ref`
  *
  * WARNING: Be sure to only call this inside a callback that is passed as a ref.
- * Otherwise, make sure to cleanup the previous {ref} if it changes. See
+ * Otherwise, make sure to cleanup the previous `ref` if it changes. See
  * https://github.com/mui/material-ui/issues/13539
  *
  * Useful if you want to expose the ref of an inner component to the public API
  * while still using it inside the component.
- * @param ref A ref callback or ref object. If anything falsy, this is a no-op.
+ *
+ * @param ref - A ref callback or ref object. If anything falsy, this is a
+ * no-op.
  */
 export const setRef = <T>(
   ref:
