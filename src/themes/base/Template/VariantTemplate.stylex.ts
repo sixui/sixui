8b237de1b034aabebc4b4a7e8cd5f72193a2389a
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-03-05 09:09:53 +0400

    feat: more templates (basic, variable, polymorphic)

--- src/themes/base/Template/VariantTemplate.stylex.ts
+++ src/themes/base/Template/VariantTemplate.stylex.ts
@@ -3,9 +3,10 @@ import stylex from '@stylexjs/stylex';
 import type { IStyleVars } from '@/helpers/types';
 import type { ITemplateStyleVarKey } from '@/components/atoms/Template';
 import { componentVars as baseComponentVars } from './Template.stylex';
+import { colorRolesVars } from '../vars/colorRoles.stylex';
 
 const vars: Partial<IStyleVars<ITemplateStyleVarKey>> = {
-  var1: '',
+  textColor: colorRolesVars.onSurfaceVariant,
 };
 
 export const componentTheme = stylex.createTheme(baseComponentVars, vars);
