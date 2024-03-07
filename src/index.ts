41a966699cecf20f392a7ed8c8e8f3b7c3d7f70c
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-03-07 10:37:30 +0400

    fix: main exports

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,57 @@
-// helpers
-export * from './helpers/ThemeProvider';
-export * from './helpers/ThemeContext';
-
-// utils
-export * from './components/utils/Elevation';
-export * from './components/utils/FocusRing';
-export * from './components/utils/Form';
-export * from './components/utils/StateLayer';
-export * from './components/utils/ComponentShowcase';
-
 // atoms
-export * from './components/atoms/Button';
-export * from './components/atoms/Chip';
-export * from './components/atoms/CircularProgressIndicator';
-export * from './components/atoms/Fab';
-export * from './components/atoms/Field';
-export * from './components/atoms/IconButton';
-export * from './components/atoms/Placeholder';
-export * from './components/atoms/Switch';
-export * from './components/atoms/TextField';
-export * from './components/atoms/Radio';
-export * from './components/atoms/Checkbox';
-export * from './components/atoms/Item';
-export * from './components/atoms/ListItem';
-export * from './components/atoms/List';
-export * from './components/atoms/Divider';
-export * from './components/atoms/Paper';
-export * from './components/atoms/Tab';
-export * from './components/atoms/TabList';
-export * from './components/atoms/Tabs';
-export * from './components/atoms/Breadcrumbs';
-export * from './components/atoms/Typography';
 export * from './components/atoms/Avatar';
+export * from './components/atoms/Badge';
+export * from './components/atoms/Breadcrumbs';
+export * from './components/atoms/Button';
+export * from './components/atoms/ButtonBase';
 export * from './components/atoms/Card';
 export * from './components/atoms/CardActions';
 export * from './components/atoms/CardContent';
 export * from './components/atoms/CardHeader';
 export * from './components/atoms/CardMedia';
 export * from './components/atoms/CardTitle';
+export * from './components/atoms/Checkbox';
+export * from './components/atoms/Chip';
+export * from './components/atoms/CircularProgressIndicator';
+export * from './components/atoms/Dialog';
+export * from './components/atoms/Divider';
+export * from './components/atoms/Fab';
+export * from './components/atoms/Field';
+export * from './components/atoms/IconButton';
+export * from './components/atoms/Item';
+export * from './components/atoms/List';
+export * from './components/atoms/ListItem';
+export * from './components/atoms/Paper';
+export * from './components/atoms/Placeholder';
+export * from './components/atoms/Radio';
+export * from './components/atoms/RadioGroup';
+export * from './components/atoms/Scrim';
+export * from './components/atoms/Switch';
+export * from './components/atoms/Tab';
+export * from './components/atoms/TabList';
+export * from './components/atoms/TabPanel';
+export * from './components/atoms/Tabs';
+export * from './components/atoms/TextField';
+export * from './components/atoms/Typography';
+
+// utils
+export * from './components/utils/Anchored';
+export * from './components/utils/ComponentShowcase';
+export * from './components/utils/Elevation';
+export * from './components/utils/Fade';
+export * from './components/utils/FocusRing';
+export * from './components/utils/Form';
+export * from './components/utils/StateLayer';
+export * from './components/utils/VisualState';
 
 // hooks
 export * from './hooks/useForkRef';
+export * from './hooks/useLoaded';
+export * from './hooks/usePrevious';
+export * from './hooks/useVisualState';
+
+// helpers
+export * from './helpers/ThemeProvider';
+export * from './helpers/ThemeContext';
 
 export { theme as baseTheme } from './themes/base';
