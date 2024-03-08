7024189d2dc0c3b2f5a51e5b53e54c3d7f39197f
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-03-08 11:54:59 +0400

    fix: smaller icon size for dialog headline

--- src/themes/base/Dialog/Dialog.stylex.ts
+++ src/themes/base/Dialog/Dialog.stylex.ts
@@ -16,7 +16,7 @@ const vars: Partial<IStyleVars<IDialogStyleVarKey>> = {
 
   // icon
   iconColor: colorRolesVars.secondary,
-  iconSize: '24px',
+  iconSize: '18px',
 
   // headline
   headlineColor: colorRolesVars.onSurface,
