269bf1effb302810f671347552f6f83556fd4d93
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-04-14 02:11:35 +0200

    feat(DisclosurePanel): removed trailing space

--- src/themes/base/DisclosurePanel/DisclosurePanel.styles.ts
+++ src/themes/base/DisclosurePanel/DisclosurePanel.styles.ts
@@ -4,12 +4,14 @@ import stylex from '@stylexjs/stylex';
 import type { IStyles } from '@/helpers/types';
 import type { IDisclosurePanelStyleKey } from '@/components/atoms/DisclosurePanel';
 import { componentVars as vars } from './DisclosurePanel.stylex';
+import { componentVars as listItemVars } from '../ListItem/ListItem.stylex';
 
 type IDisclosurePanelStyles = IStyles<IDisclosurePanelStyleKey>;
 export const styles: MapNamespaces<IDisclosurePanelStyles> =
   stylex.create<IDisclosurePanelStyles>({
     host: {
       paddingTop: '1rem',
+      paddingLeft: listItemVars.leadingSpace,
       color: vars.textColor,
     },
     host$collapsed: {
