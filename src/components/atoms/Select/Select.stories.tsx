85f70ba2ab97d50faea9fbef9db9c9b596c37a84
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-06-29 01:50:48 +0400

    wip: Select

--- src/components/atoms/Select/Select.stories.tsx
+++ src/components/atoms/Select/Select.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { sbHandleEvent } from '@/helpers/sbHandleEvent';
+import { ListItem } from '@/components/atoms/ListItem';
+import { TOP_100_MOVIES } from '@/components/atoms/FilteredList/movies';
 import { SelectExample, type ISelectExampleProps } from './SelectExample';
 
 const meta = {
@@ -25,6 +27,54 @@ export const CanFilter: IStory = {
   args: {
     ...defaultArgs,
     canFilter: true,
+  },
+};
+
+export const DefaultQuery: IStory = {
+  render: (props) => <SelectExample {...props} />,
+  args: {
+    ...defaultArgs,
+    canFilter: true,
+    defaultQuery: 'king',
+  },
+};
+
+export const CanCreate: IStory = {
+  render: (props) => <SelectExample {...props} />,
+  args: {
+    ...defaultArgs,
+    canFilter: true,
+    canCreate: true,
+    defaultQuery: 'king',
+  },
+};
+
+export const NoResults: IStory = {
+  render: (props) => <SelectExample {...props} />,
+  args: {
+    ...defaultArgs,
+    canFilter: true,
+    defaultQuery: 'My great movie',
+  },
+};
+
+export const InitialContent: IStory = {
+  render: (props) => <SelectExample {...props} />,
+  args: {
+    ...defaultArgs,
+    canFilter: true,
+    initialContent: (
+      <ListItem disabled>{TOP_100_MOVIES.length} items loaded.</ListItem>
+    ),
+  },
+};
+
+export const Disabled: IStory = {
+  render: (props) => <SelectExample {...props} />,
+  args: {
+    ...defaultArgs,
+    canFilter: true,
+    disabled: true,
   },
 };
 
--- src/components/atoms/Select/SelectExample.tsx
+++ src/components/atoms/Select/SelectExample.tsx
@@ -13,23 +13,27 @@ import { Select, type ISelectProps } from './Select';
 export type ISelectExampleProps = IOmit<
   ISelectProps<IMovie>,
   'items' | 'itemRenderer'
->;
+> & {
+  canCreate?: boolean;
+};
 
 export const SelectExample = (props: ISelectExampleProps): React.ReactNode => {
+  const { canCreate, ...rest } = props;
+
   return (
     <Select<IMovie>
       items={TOP_100_MOVIES}
       itemRenderer={renderMovieListItem}
       itemsEqual={areMoviesEqual}
       itemPredicate={filterMovie}
-      createNewItemFromQuery={createMovie}
-      createNewItemRenderer={renderCreateMovieListItem}
+      createNewItemFromQuery={canCreate ? createMovie : undefined}
+      createNewItemRenderer={canCreate ? renderCreateMovieListItem : undefined}
       getFieldProps={(value) => ({
         variant: 'outlined',
-        label: 'Film',
+        label: 'Choose a film',
         children: value?.title,
       })}
-      {...props}
+      {...rest}
     />
   );
 };
