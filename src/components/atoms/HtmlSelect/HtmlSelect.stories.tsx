64e6d4d35825211db990b643854be0b141a2febc
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-06-30 16:49:59 +0400

    wip: HtmlSelect

---
+++ src/components/atoms/HtmlSelect/HtmlSelect.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { sbHandleEvent } from '@/helpers/sbHandleEvent';
+import { HtmlSelect, type IHtmlSelectProps } from './HtmlSelect';
+
+const meta = {
+  component: HtmlSelect,
+} satisfies Meta<typeof HtmlSelect>;
+
+type IStory = StoryObj<typeof meta>;
+
+const defaultArgs = {
+  options: [
+    {
+      label: 'Dog',
+      value: 'dog',
+    },
+    {
+      label: 'Cat',
+      value: 'cat',
+    },
+    {
+      label: 'Hamster',
+      value: 'hamster',
+    },
+    {
+      label: 'Parrot',
+      value: 'parrot',
+    },
+    {
+      label: 'Spider',
+      value: 'spider',
+    },
+    {
+      label: 'Goldfish',
+      value: 'goldfish',
+    },
+  ],
+  onChange: (...args) => void sbHandleEvent('onChange', ...args),
+} satisfies Partial<IHtmlSelectProps>;
+
+export const Basic: IStory = {
+  render: (props) => <HtmlSelect {...props} />,
+  args: defaultArgs,
+};
+
+export default meta;
