64b7839badcad5e4a909e80cb31eca0415db8be1
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-07-18 16:08:53 +0400

    fix(isObject): bug

--- src/helpers/isObject.ts
+++ src/helpers/isObject.ts
@@ -1,7 +1,4 @@
 import type { IAny } from './types';
 
-export const isObject = (value: IAny): value is object => {
-  const type = typeof value;
-
-  return value !== null && (type === 'object' || type === 'function');
-};
+export const isObject = (value: IAny): value is object =>
+  typeof value === 'object' && value !== null && !Array.isArray(value);
