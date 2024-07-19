b31af9955d74e61dca0fa1c175c6bac82aa4255f
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-07-19 20:29:34 +0400

    feat: color picker

--- src/components/ColorTag/ColorTag.types.ts
+++ src/components/ColorTag/ColorTag.types.ts
@@ -1,8 +1,6 @@
 import type { IColor, IContainerProps } from '@/helpers/types';
 import type { IColorTagStylesKey } from './ColorTag.styles';
 
-export const COLOR_BUTTON_DEFAULT_TAG = 'button';
-
 export type IColorTagProps = IContainerProps<IColorTagStylesKey> & {
   children?: React.ReactNode;
   label?: React.ReactNode;
