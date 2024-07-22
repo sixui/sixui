8b555ad40a72a81dc56996f997f8bc85014c628c
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-07-22 17:03:24 +0400

    feat: DynamicThemeProvider inherit from Theme context

--- src/components/Theme/DynamicThemeProvider.tsx
+++ src/components/Theme/DynamicThemeProvider.tsx
@@ -2,6 +2,7 @@ import type { IDynamicThemeProviderProps } from './DynamicThemeProvider.types';
 import { generateThemeFromSourceColor } from '@/helpers/colors/generateThemeFromSourceColor';
 import { IDynamicSchemeVariant } from '@/helpers/colors/getMaterialDynamicSchemeClass';
 import { ThemeProvider } from './ThemeProvider';
+import { useThemeContext } from './useThemeContext';
 
 export const DynamicThemeProvider: React.FC<IDynamicThemeProviderProps> = (
   props,
@@ -10,14 +11,24 @@ export const DynamicThemeProvider: React.FC<IDynamicThemeProviderProps> = (
     sourceColor,
     schemeVariant = IDynamicSchemeVariant.sixui,
     contrast = 0.0,
+    settings,
+    componentsStyles,
     ...other
   } = props;
 
+  const themeContext = useThemeContext();
   const theme = generateThemeFromSourceColor(
     sourceColor,
     schemeVariant,
     contrast,
   );
 
-  return <ThemeProvider {...other} theme={theme} />;
+  return (
+    <ThemeProvider
+      {...other}
+      settings={settings ?? themeContext.settings}
+      componentsStyles={componentsStyles ?? themeContext.componentsStyles}
+      theme={theme}
+    />
+  );
 };
