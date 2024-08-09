d8906d60a874966f6947e6e5c213fbfca4f54ec4
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-08-09 17:06:04 +0400

    feat(Skeleton): polymorphic try

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -3,6 +3,7 @@ import { forwardRef, useRef } from 'react';
 
 import type { ISkeletonProps } from './Skeleton.types';
 import { random } from '~/helpers/random';
+import { createPolymorphicComponent } from '~/helpers/react/polymorphicComponentTypes';
 import { useStyles } from '~/hooks/useStyles';
 import { Base } from '../Base';
 import { skeletonStyles } from './Skeleton.styles';
@@ -14,57 +15,59 @@ const staticStyles = stylex.create({
   }),
 });
 
-export const Skeleton = forwardRef<HTMLDivElement, ISkeletonProps>(
-  function Skeleton(props, forwardedRef) {
-    const {
-      styles,
-      sx,
-      children,
-      loaded,
-      variant = 'rectangular',
-      animation: animationProp = 'pulse',
-      length: lengthProp,
-      hasError,
-      ...other
-    } = props;
+export const Skeleton = createPolymorphicComponent<'div', ISkeletonProps>(
+  forwardRef<HTMLDivElement, ISkeletonProps>(
+    function Skeleton(props, forwardedRef) {
+      const {
+        styles,
+        sx,
+        children,
+        loaded,
+        variant = 'rectangular',
+        animation: animationProp = 'pulse',
+        length: lengthProp,
+        hasError,
+        ...other
+      } = props;
 
-    const { combineStyles, getStyles, globalStyles } = useStyles({
-      name: 'Skeleton',
-      styles: [skeletonStyles, styles],
-    });
+      const { combineStyles, getStyles, globalStyles } = useStyles({
+        name: 'Skeleton',
+        styles: [skeletonStyles, styles],
+      });
 
-    const animation = hasError ? undefined : animationProp;
-    const lengthRef = useRef(
-      typeof lengthProp === 'object' ? random(lengthProp) : lengthProp,
-    );
+      const animation = hasError ? undefined : animationProp;
+      const lengthRef = useRef(
+        typeof lengthProp === 'object' ? random(lengthProp) : lengthProp,
+      );
 
-    return loaded ? (
-      children
-    ) : (
-      <Base
-        {...other}
-        sx={[
-          skeletonTheme,
-          globalStyles,
-          variant === 'rectangular'
-            ? lengthRef.current !== undefined
-              ? staticStyles.length(lengthRef.current)
-              : !children
-                ? staticStyles.length()
-                : undefined
-            : undefined,
-          combineStyles(
-            'host',
-            hasError ? 'host$error' : null,
-            `host$${variant}`,
-            !!animation && `animation$${animation}`,
-          ),
-          sx,
-        ]}
-        ref={forwardedRef}
-      >
-        <div {...getStyles('hidden')}>{children ?? '%'}</div>
-      </Base>
-    );
-  },
+      return loaded ? (
+        children
+      ) : (
+        <Base
+          {...other}
+          sx={[
+            skeletonTheme,
+            globalStyles,
+            variant === 'rectangular'
+              ? lengthRef.current !== undefined
+                ? staticStyles.length(lengthRef.current)
+                : !children
+                  ? staticStyles.length()
+                  : undefined
+              : undefined,
+            combineStyles(
+              'host',
+              hasError ? 'host$error' : null,
+              `host$${variant}`,
+              !!animation && `animation$${animation}`,
+            ),
+            sx,
+          ]}
+          ref={forwardedRef}
+        >
+          <div {...getStyles('hidden')}>{children ?? '%'}</div>
+        </Base>
+      );
+    },
+  ),
 );
