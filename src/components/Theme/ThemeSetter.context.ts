3ef7bff2db6e9dec409fb290017e92899454b2a8
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-08-11 12:28:36 +0400

    fix(ThemeProvider): setTheme extends current theme

--- src/components/Theme/ThemeSetter.context.ts
+++ src/components/Theme/ThemeSetter.context.ts
@@ -3,7 +3,7 @@ import { createContext } from 'react';
 import type { ITheme } from '~/themes/base';
 
 export type IThemeSetterContextValue = {
-  setTheme: (theme: ITheme | undefined) => void;
+  setTheme: (theme: Partial<ITheme> | undefined) => void;
 };
 
 export const ThemeSetterContext = createContext<
