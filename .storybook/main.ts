7a2e90a23623fc2c26fc93a2347448444863f4de
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-08-21 16:59:11 +0400

    fix: ts types + storybook migration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,16 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials', '@chromatic-com/storybook'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+  },
+  core: {
+    disableTelemetry: true, // 👈 Disables telemetry
   },
 };
 
