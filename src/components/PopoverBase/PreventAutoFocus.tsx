aa1bc5b2a20362cd8e4a7c4af0a99d9f5985214e
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-08-22 18:19:39 +0400

    fix(PopoverBase): preventAutoFocus prop

--- src/components/PopoverBase/PreventAutoFocus.tsx
+++ src/components/PopoverBase/PreventAutoFocus.tsx
@@ -6,11 +6,5 @@ export const PreventAutoFocus: React.FC = () => {
   // This is a hack to prevent the first focusable element from being focused
   // when the side sheet is opened.
 
-  return (
-    <button
-      aria-hidden
-      type='button'
-      {...stylex.props(commonStyles.outOfScreen)}
-    />
-  );
+  return <button type='button' {...stylex.props(commonStyles.outOfScreen)} />;
 };
