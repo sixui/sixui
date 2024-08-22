2f097d5e8c9aae9b497ef58cafaff169212005e8
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-08-22 12:56:50 +0400

    fix(AppLayoutFooter): no height

--- src/components/AppLayout/AppLayout.styles.ts
+++ src/components/AppLayout/AppLayout.styles.ts
@@ -1,10 +1,6 @@
 import stylex from '@stylexjs/stylex';
 
-import { colorSchemeTokens } from '~/themes/base/colorScheme.stylex';
-
 export type IAppLayoutStylesKey = keyof typeof appLayoutStyles;
 export const appLayoutStyles = stylex.create({
-  host: {
-    backgroundColor: colorSchemeTokens.surface,
-  },
+  host: {},
 });
--- src/components/AppLayout/AppLayoutFooter/AppLayoutFooter.styles.ts
+++ src/components/AppLayout/AppLayoutFooter/AppLayoutFooter.styles.ts
@@ -3,13 +3,11 @@ import stylex from '@stylexjs/stylex';
 import { zIndexTokens } from '~/themes/base/zIndex.stylex';
 import { outlineTokens } from '~/themes/base/outline.stylex';
 import { colorSchemeTokens } from '~/themes/base/colorScheme.stylex';
-import { appLayoutTokens } from '../AppLayout.stylex';
 import { spacingTokens } from '~/themes/base/spacing.stylex';
 
 export type IAppLayoutFooterStylesKey = keyof typeof appLayoutFooterStyles;
 export const appLayoutFooterStyles = stylex.create({
   host: {
-    height: appLayoutTokens.footerHeight,
     borderTopWidth: outlineTokens.width$xs,
     borderTopStyle: 'solid',
     borderTopColor: colorSchemeTokens.outline,
