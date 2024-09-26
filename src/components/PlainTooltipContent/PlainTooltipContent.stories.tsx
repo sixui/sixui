568dbff66ab5310ec7b0f4893c8c709267812faa
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-09-26 15:30:56 +0400

    wip

--- src/components/PlainTooltipContent/PlainTooltipContent.stories.tsx
+++ src/components/PlainTooltipContent/PlainTooltipContent.stories.tsx
@@ -17,6 +17,7 @@ const PlainTooltipContentShowcase = makeComponentShowcase(PlainTooltipContent);
 const defaultArgs = {
   supportingText:
     'Grant value is calculated using the closing stock price from the day before the grant date. Amounts do not reflect tax withholding.',
+  corner: '$none',
 } satisfies Partial<IPlainTooltipContentProps>;
 
 export const Standard: IStory = {
--- src/components/PlainTooltipContent/PlainTooltipContent.types.ts
+++ src/components/PlainTooltipContent/PlainTooltipContent.types.ts
@@ -1,17 +1,18 @@
 import type { IComponentFactory } from '~/utils/component/componentFactory';
 import type { IComponentThemeProps } from '~/utils/styles/useComponentTheme';
 import type { IBoxProps } from '../Box';
+import type { IPaperOwnProps } from '../Paper';
 import type {
   IPlainTooltipContentThemeFactory,
   plainTooltipContentTheme,
 } from './PlainTooltipContent.css';
 
-export type IPlainTooltipContentOwnProps = {
+export interface IPlainTooltipContentOwnProps extends IPaperOwnProps {
   supportingText: React.ReactNode;
   renderCursor?: (
     userProps?: React.HTMLAttributes<SVGSVGElement>,
   ) => React.ReactNode;
-};
+}
 
 export interface IPlainTooltipContentProps
   extends IBoxProps,
