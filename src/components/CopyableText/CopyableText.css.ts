1bd4b08e98d27e22061e4ff1457bacb2c0334e42
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2024-12-28 14:41:49 +0400

    feat: CopyableText

--- src/components/CopyableText/CopyableText.css.ts
+++ src/components/CopyableText/CopyableText.css.ts
@@ -1,15 +1,8 @@
-import { createTheme } from '@vanilla-extract/css';
-
 import type { IComponentThemeFactory } from '~/utils/styles/componentThemeFactory';
 import { componentThemeFactory } from '~/utils/styles/componentThemeFactory';
 import { createStyles } from '~/utils/styles/createStyles';
-import { cssLayers } from '../ThemeProvider';
 
 type IModifier = 'disabled';
-
-const [tokensClassName, tokens] = createTheme({
-  '@layer': cssLayers.theme,
-});
 
 const classNames = createStyles({
   label: {
@@ -19,13 +12,11 @@ const classNames = createStyles({
 
 export type ICopyableTextThemeFactory = IComponentThemeFactory<{
   styleName: keyof typeof classNames;
-  tokens: typeof tokens;
   modifier: IModifier;
 }>;
 
 export const copyableTextTheme =
   componentThemeFactory<ICopyableTextThemeFactory>({
     classNames,
-    tokensClassName,
-    tokens,
+    tokens: null,
   });
