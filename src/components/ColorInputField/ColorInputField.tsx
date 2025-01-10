9ac6800cb541f5a28c99b5e6ed48e9f42d5ffbef
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2025-01-10 20:48:32 +0400

    wip: ColorInputField quantization

--- src/components/ColorInputField/ColorInputField.tsx
+++ src/components/ColorInputField/ColorInputField.tsx
@@ -1,14 +1,21 @@
+import { useCallback, useContext, useRef, useState } from 'react';
+
 import type {
   IColorInputFieldColorPickerRendererProps,
   IColorInputFieldFactory,
 } from './ColorInputField.types';
+import { iconPhoto } from '~/assets/icons';
+import { extractPaletteFromImage } from '~/helpers/colors/extractPaletteFromImage';
 import { isValidHexColor } from '~/helpers/colors/isValidHexColor';
 import { useControlledValue } from '~/hooks/useControlledValue';
 import { componentFactory } from '~/utils/component/componentFactory';
 import { useProps } from '~/utils/component/useProps';
+import { ColorPaletteGroupContext } from '../ColorPaletteGroup';
 import { ColorTag } from '../ColorTag';
 import { HslColorPickerContent } from '../HslColorPickerContent';
+import { IconButton } from '../IconButton';
 import { PopoverBase } from '../PopoverBase';
+import { SvgIcon } from '../SvgIcon';
 import { TextInputField } from '../TextInputField';
 import { colorInputFieldTheme } from './ColorInputField.css';
 
@@ -26,9 +33,9 @@ export const ColorInputField = componentFactory<IColorInputFieldFactory>(
         alignment: 'start',
       },
       colorPickerRenderer = defaultColorPickerRenderer,
-      customPalette,
+      customPalette: customPaletteProp,
       onColorsQuantized,
-      quantizeColorCount,
+      quantizeColorCount = 8,
       value: valueProp,
       defaultValue,
       onChange,
@@ -41,56 +48,113 @@ export const ColorInputField = componentFactory<IColorInputFieldFactory>(
       name: COMPONENT_NAME,
     });
 
+    const colorPaletteGroupContext = useContext(ColorPaletteGroupContext);
+    const [quantizedColors, setQuantizedColors] = useState<Array<string>>();
+    const [quantizing, setQuantizing] = useState(false);
+    const inputFileRef = useRef<HTMLInputElement>(null);
+
     const isValidColor = isValidHexColor(value);
+    const customPalette = [
+      ...(customPaletteProp ?? []),
+      ...((colorPaletteGroupContext ? undefined : quantizedColors) ?? []),
+    ];
+
+    const handleUploadImage: React.MouseEventHandler = useCallback(
+      () => inputFileRef.current?.click(),
+      [],
+    );
+
+    const handleFileChange: React.ChangeEventHandler<HTMLInputElement> =
+      useCallback(
+        (event) => {
+          const files = event.target.files;
+          const file = files?.[0];
+          if (!file) {
+            return;
+          }
+
+          setQuantizing(true);
+
+          const fileReader = new FileReader();
+          fileReader.onload = () => {
+            const image = new Image();
+            image.src = fileReader.result as string;
+            void extractPaletteFromImage(image, quantizeColorCount)
+              .then((quantizedPalette) => {
+                setQuantizedColors(quantizedPalette);
+                setValue(quantizedPalette[0]);
+                onColorsQuantized?.(quantizedPalette);
+                colorPaletteGroupContext?.setQuantizedPalette(quantizedPalette);
+              })
+              .finally(() => setQuantizing(false));
+          };
+          fileReader.readAsDataURL(file);
+        },
+        [
+          colorPaletteGroupContext,
+          onColorsQuantized,
+          quantizeColorCount,
+          setValue,
+        ],
+      );
 
     return (
-      <PopoverBase
-        contentRenderer={({ close }) =>
-          colorPickerRenderer({
-            onClick: (_event, colorHex) => {
-              setValue(colorHex);
-              onChange?.(colorHex, true);
-              close();
-            },
-            selectedColor: value,
-            customPalette,
-          })
-        }
-        trapFocus
-        placement={placement}
-        floatingMotionProps={{
-          orientation: 'vertical',
-        }}
-        positioned
-      >
-        {({ getProps, setRef, open }) => (
-          <TextInputField
-            start={<ColorTag color={value} outlined mr="$2" onClick={open} />}
-            // FIXME:
-            end={
-              undefined
-              // <IconButton
-              //   icon={<SvgIcon icon={iconPhoto} />}
-              //   onClick={(event) => {
-              //     handleUploadImage(event);
-              //   }}
-              //   loading={isQuantizing}
-              // />
-            }
-            autoComplete="off"
-            {...other}
-            {...getProps()}
-            value={value}
-            hasError={(!!value && !isValidColor) || other.hasError}
-            onChange={(nextValue) => {
-              setValue(nextValue);
-              onChange?.(nextValue, isValidHexColor(nextValue));
-            }}
-            ref={forwardedRef}
-            containerRef={setRef}
-          />
-        )}
-      </PopoverBase>
+      <>
+        <input
+          type="file"
+          ref={inputFileRef}
+          accept="image/*"
+          style={{ display: 'none' }}
+          onChange={handleFileChange}
+        />
+
+        <PopoverBase
+          contentRenderer={({ close }) =>
+            colorPickerRenderer({
+              onClick: (_event, colorHex) => {
+                setValue(colorHex);
+                onChange?.(colorHex, true);
+                close();
+              },
+              selectedColor: value,
+              customPalette,
+            })
+          }
+          middlewares={{
+            offset: false,
+          }}
+          trapFocus
+          placement={placement}
+          floatingMotionProps={{
+            orientation: 'vertical',
+          }}
+          positioned
+        >
+          {({ getProps, setRef, open }) => (
+            <TextInputField
+              start={<ColorTag color={value} outlined mr="$2" onClick={open} />}
+              end={
+                <IconButton
+                  icon={<SvgIcon icon={iconPhoto} />}
+                  onClick={handleUploadImage}
+                  loading={quantizing}
+                />
+              }
+              autoComplete="off"
+              {...other}
+              {...getProps()}
+              value={value}
+              hasError={(!!value && !isValidColor) || other.hasError}
+              onChange={(nextValue) => {
+                setValue(nextValue);
+                onChange?.(nextValue, isValidHexColor(nextValue));
+              }}
+              ref={forwardedRef}
+              containerRef={setRef}
+            />
+          )}
+        </PopoverBase>
+      </>
     );
   },
 );
