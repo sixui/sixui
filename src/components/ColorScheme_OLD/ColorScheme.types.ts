232bed6efb232928a5bc797cb538b8fb37b997c6
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2025-01-15 10:41:57 +0400

    wip: ThemeProvider

--- src/components/ColorScheme_OLD/ColorScheme.types.ts
+++
@@ -1,1 +0,0 @@
-export type IColorSchemeProps = Record<string, never>;
--- src/components/ThemeProvider/ThemeProvider.stories.tsx
+++ src/components/ThemeProvider/ThemeProvider.stories.tsx
@@ -1,15 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { useContext } from 'react';
-import stylex from '@stylexjs/stylex';
 
 import type { IThemeProviderProps } from './ThemeProvider.types';
-import { ColorScheme } from '~/docs/ColorScheme';
 import { generateThemeFromSourceColor } from '~/helpers/colors/generateThemeFromSourceColor';
 import { isValidHexColor } from '~/helpers/colors/isValidHexColor';
-import { colorSchemeTokens } from '~/themes/base/colorScheme.stylex';
-import { spacingTokens } from '~/themes/base/spacing.stylex';
 import { ColorInputField } from '../ColorInputField';
-import { Stack } from '../Stack';
+import { ColorScheme } from '../ColorScheme';
+import { Flex } from '../Flex';
+import { Paper } from '../Paper';
 import { ThemeProvider } from './ThemeProvider';
 import { ThemeSetterContext } from './ThemeSetter.context';
 
@@ -19,15 +17,6 @@ const meta = {
 
 type IStory = StoryObj<typeof meta>;
 
-const styles = stylex.create({
-  inner: {
-    padding: spacingTokens.padding$4,
-    borderWidth: 1,
-    borderStyle: 'dashed',
-    borderColor: colorSchemeTokens.outlineVariant,
-  },
-});
-
 const defaultArgs = {} satisfies Partial<IThemeProviderProps>;
 
 const DynamicThemeProviderDemo: React.FC = () => {
@@ -35,20 +24,22 @@ const DynamicThemeProviderDemo: React.FC = () => {
 
   const handleChange = (color: string): void => {
     themeSetterContext?.setTheme({
-      colorScheme: isValidHexColor(color)
-        ? generateThemeFromSourceColor(color)
-        : undefined,
+      tokens: {
+        colorScheme: isValidHexColor(color)
+          ? generateThemeFromSourceColor(color)
+          : undefined,
+      },
     });
   };
 
   return (
-    <Stack gap={2}>
+    <Flex direction="column" gap="$2">
       <ColorInputField onChange={handleChange} clearable />
 
-      <div {...stylex.props(styles.inner)}>
+      <Paper p="$4" outline="$xs" outlineStyle="dashed">
         <ColorScheme />
-      </div>
-    </Stack>
+      </Paper>
+    </Flex>
   );
 };
 
