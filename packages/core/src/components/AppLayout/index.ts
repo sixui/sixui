3c9ae26ebc52c7ade56752bc0e601830f73df068
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2025-02-17 21:47:29 +0400

    wip: AppLayoutFeedBody

--- packages/core/src/components/AppLayout/index.ts
+++ packages/core/src/components/AppLayout/index.ts
@@ -2,9 +2,12 @@ export * from './AppLayout.types';
 export * from './AppLayout';
 
 export * from './AppLayoutBody';
+export * from './AppLayoutFeedBody';
 export * from './AppLayoutFooter';
-export * from './AppLayoutTopBar';
+export * from './AppLayoutListDetailBody';
 export * from './AppLayoutNavigationBar';
 export * from './AppLayoutNavigationDrawer';
 export * from './AppLayoutNavigationRail';
 export * from './AppLayoutSideSheet';
+export * from './AppLayoutSupportingPaneBody';
+export * from './AppLayoutTopBar';
