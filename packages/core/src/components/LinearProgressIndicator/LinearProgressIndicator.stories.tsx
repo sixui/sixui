856f53a11b6349dcd6692862db63eacdbdfe0515
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2025-02-27 18:47:46 +0400

    fix: LinearProgressIndicator stories

--- packages/core/src/components/LinearProgressIndicator/LinearProgressIndicator.stories.tsx
+++ packages/core/src/components/LinearProgressIndicator/LinearProgressIndicator.stories.tsx
@@ -3,7 +3,6 @@ import type { Meta, StoryObj } from '@storybook/react';
 import type { IComponentPresentation } from '~/components/ComponentShowcase';
 import type { ILinearProgressIndicatorProps } from './LinearProgressIndicator.types';
 import { componentShowcaseFactory } from '~/components/ComponentShowcase';
-import { Flex } from '~/components/Flex';
 import { LinearProgressIndicator } from './LinearProgressIndicator';
 
 const meta = {
@@ -28,6 +27,8 @@ const LinearProgressIndicatorShowcase = componentShowcaseFactory(
 export const Variants: IStory = {
   render: (props) => (
     <LinearProgressIndicatorShowcase
+      horizontalAlign="start"
+      verticalAlign="center"
       props={props}
       cols={[{ props: { value: undefined } }, { props: { value: 0.75 } }]}
       rows={rows}
@@ -36,42 +37,20 @@ export const Variants: IStory = {
   args: defaultArgs,
 };
 
-const LinearProgressIndicatorWithTextShowcase = componentShowcaseFactory(
-  (props: ILinearProgressIndicatorProps) => (
-    <Flex gap="$2">
-      <LinearProgressIndicator {...props} />{' '}
-      <LinearProgressIndicator {...props} value={0.75} />
-    </Flex>
-  ),
-);
-
 export const Scales: IStory = {
   render: (props) => (
-    <LinearProgressIndicatorWithTextShowcase
+    <LinearProgressIndicatorShowcase
+      horizontalAlign="start"
+      verticalAlign="center"
       props={props}
-      cols={[
+      rows={[
         { legend: 'Extra small', props: { scale: 'xs' } },
         { legend: 'Small', props: { scale: 'sm' } },
         { legend: 'Medium', props: { scale: 'md' } },
         { legend: 'Large', props: { scale: 'lg' } },
         { legend: 'Extra large', props: { scale: 'xl' } },
       ]}
-    />
-  ),
-  args: defaultArgs,
-};
-
-export const FontSizes: IStory = {
-  render: (props) => (
-    <LinearProgressIndicatorWithTextShowcase
-      props={props}
-      cols={[
-        { legend: '2', props: { fz: '$2' } },
-        { legend: '4', props: { fz: '$4' } },
-        { legend: '8', props: { fz: '$8' } },
-        { legend: '16', props: { fz: '$16' } },
-        { legend: '32', props: { fz: '$32' } },
-      ]}
+      cols={[{ props: { value: undefined } }, { props: { value: 0.75 } }]}
     />
   ),
   args: defaultArgs,
