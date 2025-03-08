70aae785f0982c537b973a9500eb63925b9bc79a
Olivier Pascal <2182039+olivierpascal@users.noreply.github.com>
2025-03-08 10:09:24 +0400

    doc: responsive padding in story wrapper

--- packages/core/.storybook/preview.css.ts
+++ packages/core/.storybook/preview.css.ts
@@ -1,21 +1,16 @@
 import { style } from '@vanilla-extract/css';
 
+import { responsiveContainerQuery } from '~/utils/css';
 import { px } from '~/utils/css/px';
 import { themeTokens } from '~/components/Theme/theme.css';
 
 export const classNames = {
   wrapper: style({
     width: '100%',
-
-    '@media': {
-      '(prefers-color-scheme: dark)': {
-        // flexDirection: 'column-reverse',
-      },
-    },
   }),
   storyWrapper: style({
     position: 'relative',
-    padding: 96,
+    padding: px(96),
     backgroundColor: themeTokens.colorScheme.surfaceContainerLowest,
     backgroundImage: `radial-gradient(${themeTokens.colorScheme.outlineVariant} max(0.5px, ${px(0.5)}), transparent 0)`,
     backgroundSize: `${px(10)} ${px(10)}`,
@@ -29,6 +24,24 @@ export const classNames = {
       },
       '(max-width: 1280px)': {
         padding: 72,
+      },
+    },
+
+    '@container': {
+      [responsiveContainerQuery({ size: 'compact' })]: {
+        padding: px(16),
+      },
+      [responsiveContainerQuery({ size: 'medium' })]: {
+        padding: px(32),
+      },
+      [responsiveContainerQuery({ size: 'expanded' })]: {
+        padding: px(48),
+      },
+      [responsiveContainerQuery({ size: 'large' })]: {
+        padding: px(64),
+      },
+      [responsiveContainerQuery({ size: 'extraLarge' })]: {
+        padding: px(80),
       },
     },
   }),
